9d188853728ae77818d6b0be3bf68116da10cb00
wcary2 <43153826+wcary2@users.noreply.github.com>
2018-09-18 16:14:01 -0600

    Code Files V1

---
+++ Source.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string fileName;
+	cout << "Enter File Name: ";
+	cin >> fileName;
+	ifstream file(fileName);
+	if (file.is_open()) {
+		int count = 0;
+		int firstNumber, secondNumber, thirdNumber, lastNumber;
+		while (!file.eof()) {
+			if (file >> thirdNumber) {
+				count++;
+				if (count == 1) {
+					firstNumber = thirdNumber;
+					secondNumber = thirdNumber;
+				}
+			}
+			else {
+				file.clear();
+				file.ignore();
+			}
+			if (file >> lastNumber) {
+				count++;
+				if (count == 2) {
+					secondNumber = lastNumber;
+				}
+			}
+			else {
+				file.clear();
+				file.ignore();
+			}
+		}
+		cout << "Count: " << count << endl;
+		cout << "First Number: " << firstNumber << endl << "Second Number: " << secondNumber << endl;
+		cout << "Second to Last Number: " << thirdNumber << endl << "Last Number: " << lastNumber << endl;
+	}
+	cin >> fileName;
+	return 0;
+}
+
+/*void test(ifstream file) {
+	if (file.is_open()) {
+		int count = 0;
+		int firstNumber, secondNumber, thirdNumber, lastNumber;
+		while (!file.fail()) {
+			if (file >> thirdNumber) {
+				count++;
+				if (count = 1) {
+					firstNumber = thirdNumber;
+					secondNumber = firstNumber;
+				}
+			}
+			else {
+				file.clear();
+				file.ignore();
+			}
+			if (file >> lastNumber) {
+				count++;
+				if (count == 2) {
+					secondNumber = lastNumber;
+				}
+			}
+			else {
+				file.clear();
+				file.ignore();
+			}
+		}
+	}
+}*/
