0456a1fff3acc6142126f1a5eeab3557decd33e7
wcary2 <43153826+wcary2@users.noreply.github.com>
2018-09-23 01:19:59 -0600

    Add files via upload
    better version

--- Source.cpp
+++ Source.cpp
@@ -11,34 +11,25 @@ int main() {
 	ifstream file(fileName);
 	if (file.is_open()) {
 		int count = 0;
-		int firstNumber, secondNumber, thirdNumber, lastNumber;
-		while (!file.eof()) {
-			if (file >> thirdNumber) {
-				count++;
+		double firstNumber, secondNumber, thirdNumber, lastNumber;
+		double temp;
+		while (file >> temp) {
+			if (!file.fail()) {
+				if (count == 0) { //if there is only one number in the file, all stored values will be equivalent to that number
+					firstNumber = temp;
+					secondNumber = temp;
+					thirdNumber = temp;
+					lastNumber = temp;
+				}
 				if (count == 1) {
-					firstNumber = thirdNumber;
-					secondNumber = thirdNumber;
+					secondNumber = temp;
+					thirdNumber = temp;
+					lastNumber = temp;
 				}
-			}
-			else {
-				file.clear();
-				file.ignore();
-			}
-			if (file >> lastNumber) {
+				thirdNumber = lastNumber;
+				lastNumber = temp;
 				count++;
-				if (count == 2) {
-					secondNumber = lastNumber;
-				}
 			}
-			else {
-				file.clear();
-				file.ignore();
-			}
-		}
-		if ((count % 2) == 1) {
-			int temp = lastNumber;
-			lastNumber = thirdNumber;
-			thirdNumber = temp;
 		}
 		cout << "Count: " << count << endl;
 		cout << "First Number: " << firstNumber << endl << "Second Number: " << secondNumber << endl;
@@ -48,33 +39,3 @@ int main() {
 	cin >> fileName;
 	return 0;
 }
-
-/*void test(ifstream file) {
-	if (file.is_open()) {
-		int count = 0;
-		int firstNumber, secondNumber, thirdNumber, lastNumber;
-		while (!file.fail()) {
-			if (file >> thirdNumber) {
-				count++;
-				if (count = 1) {
-					firstNumber = thirdNumber;
-					secondNumber = firstNumber;
-				}
-			}
-			else {
-				file.clear();
-				file.ignore();
-			}
-			if (file >> lastNumber) {
-				count++;
-				if (count == 2) {
-					secondNumber = lastNumber;
-				}
-			}
-			else {
-				file.clear();
-				file.ignore();
-			}
-		}
-	}
-}*/
